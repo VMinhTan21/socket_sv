847a3050986a847d2f2e098a392725463f090d3f
VMinhTan <vienminhtanmd@gmail.com>
2023-07-13 23:40:05 +0700

    update origin

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 const http = require('http')
 const cors = require('cors')
 
+const PORT = process.env.PORT || process.env.port || 4000
+
 const { Server } = require('socket.io')
 
 const server = http.createServer(app)
@@ -11,13 +13,14 @@ app.use(cors())
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        // origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'http://final1-repo.vercel.app', 'http://final1-repo-test.vercel.app'],
         methods: ['GET', 'POST']
     }
 })
 
-server.listen(4000, () => {
-    console.log("SERVER IS RUNNING")
+server.listen(PORT, () => {
+    console.log("SERVER IS RUNNING IN ", PORT)
 })
 
 io.on('connection', (socket) => {
